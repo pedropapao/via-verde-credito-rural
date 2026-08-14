88a3053fd1fff943655fba594feb6a36995710de
pedropapao <pedroxboxpay@gmail.com>
2026-08-14 15:42:53 -0300

    Ativa painel somente visualização e checklists

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	srv := &http.Server{
 		Addr:              ":" + cfg.Port,
-		Handler:           app.routes360(),
+		Handler:           app.routesViewer(),
 		ReadHeaderTimeout: 10 * time.Second,
 		ReadTimeout:       30 * time.Second,
 		WriteTimeout:      60 * time.Second,
