dbed4c5ad8169b3f625d397f2430084ee4b44a8a
pedropapao <pedroxboxpay@gmail.com>
2026-08-19 14:55:03 -0300

    Restringe Central CAR ao perfil proprietário

--- car_routes.go
+++ car_routes.go
@@ -5,8 +5,8 @@ import "net/http"
 func (a *App) routesManagerCAR() http.Handler {
 	base := a.routesManagerReliable()
 	mux := http.NewServeMux()
-	mux.Handle("GET /car", securityHeaders(recoverer(logger(a.withAuth(http.HandlerFunc(a.carPage))))))
-	mux.Handle("GET /car/kml", securityHeaders(recoverer(logger(a.withAuth(http.HandlerFunc(a.carKML))))))
+	mux.Handle("GET /car", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carPage))))))
+	mux.Handle("GET /car/kml", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carKML))))))
 	mux.Handle("/", base)
 	return mux
 }
