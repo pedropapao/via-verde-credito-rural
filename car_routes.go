505682f364f7f4be514a4bc30c7c46e1bd6ea5a6
pedropapao <pedroxboxpay@gmail.com>
2026-08-19 16:19:17 -0300

    Adiciona rotas do demonstrativo CAR gratuito

--- car_routes.go
+++ car_routes.go
@@ -7,6 +7,8 @@ func (a *App) routesManagerCAR() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("GET /car", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carPage))))))
 	mux.Handle("GET /car/kml", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carKML))))))
+	mux.Handle("GET /car/demonstrativo", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoPDF))))))
+	mux.Handle("GET /car/demonstrativo.json", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoJSON))))))
 	mux.Handle("/", base)
 	return mux
 }
