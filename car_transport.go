d943bdbb419e2e2d92f7d4eea14877c3e68772b3
pedropapao <pedroxboxpay@gmail.com>
2026-08-19 15:11:40 -0300

    Restringe compatibilidade TLS do SICAR a suites TLS 1.2 conhecidas

--- car_transport.go
+++ car_transport.go
@@ -24,36 +24,22 @@ func init() {
 	normal := base.Clone()
 	legacy := base.Clone()
 	legacy.TLSClientConfig = &tls.Config{
-		MinVersion:   tls.VersionTLS12,
-		CipherSuites: sicarCipherSuites(),
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
 	}
 
 	http.DefaultTransport = &sicarCompatibleTransport{normal: normal, legacy: legacy}
-}
-
-func sicarCipherSuites() []uint16 {
-	ids := make([]uint16, 0, 16)
-	seen := map[uint16]bool{}
-	for _, suite := range tls.CipherSuites() {
-		if !seen[suite.ID] {
-			ids = append(ids, suite.ID)
-			seen[suite.ID] = true
-		}
-	}
-	// Suites RSA ainda implementadas pelo Go, mas removidas da seleção padrão
-	// em versões recentes. Mantemos apenas AES; 3DES não é reabilitado.
-	for _, id := range []uint16{
-		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	} {
-		if !seen[id] {
-			ids = append(ids, id)
-			seen[id] = true
-		}
-	}
-	return ids
 }
 
 func (t *sicarCompatibleTransport) RoundTrip(req *http.Request) (*http.Response, error) {
