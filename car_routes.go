5f15100bb4c2c2378914529189c20777f063e599
pedropapao <pedroxboxpay@gmail.com>
2026-08-24 08:18:52 -0300

    Adiciona rotas de cadastro e consulta de clientes

--- car_routes.go
+++ car_routes.go
@@ -9,6 +9,8 @@ func (a *App) routesManagerCAR() http.Handler {
 	mux.Handle("GET /car/kml", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carKML))))))
 	mux.Handle("GET /car/demonstrativo", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoPDF))))))
 	mux.Handle("GET /car/demonstrativo.json", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoJSON))))))
+	mux.Handle("GET /clients", securityHeaders(recoverer(logger(a.withAuth(http.HandlerFunc(a.managerClientsList))))))
+	mux.Handle("POST /clients", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.clientCreate))))))
 	mux.Handle("/", base)
 	return mux
 }
