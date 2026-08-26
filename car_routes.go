49d81353d46f1b45d145c3ad540afec44803706b
pedropapao <pedroxboxpay@gmail.com>
2026-08-26 07:54:23 -0300

    Remove Consulta Rural por CPF/CNPJ

--- car_routes.go
+++ car_routes.go
@@ -9,7 +9,6 @@ func (a *App) routesManagerCAR() http.Handler {
 	mux.Handle("GET /car/kml", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carKML))))))
 	mux.Handle("GET /car/demonstrativo", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoPDF))))))
 	mux.Handle("GET /car/demonstrativo.json", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoJSON))))))
-	mux.Handle("GET /documents", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.ruralLookupPage))))))
 
 	mux.Handle("GET /clients", securityHeaders(recoverer(logger(a.withAuth(http.HandlerFunc(a.managerClientsList))))))
 	mux.Handle("POST /clients", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.managerClientCreate))))))
