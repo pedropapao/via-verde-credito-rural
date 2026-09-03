61d45755677ed009c928fc7c1697cfed8e559572
pedropapao <pedroxboxpay@gmail.com>
2026-09-03 16:47:15 -0300

    Expõe diagnóstico seguro do banco no health check

--- car_routes.go
+++ car_routes.go
@@ -5,6 +5,7 @@ import "net/http"
 func (a *App) routesManagerCAR() http.Handler {
 	base := a.routesManagerReliable()
 	mux := http.NewServeMux()
+	mux.Handle("GET /health", securityHeaders(recoverer(logger(http.HandlerFunc(a.healthDB)))))
 	mux.Handle("GET /car", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carPage))))))
 	mux.Handle("GET /car/kml", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carKML))))))
 	mux.Handle("GET /car/demonstrativo", securityHeaders(recoverer(logger(a.ownerOnly(http.HandlerFunc(a.carDemonstrativoPDF))))))
